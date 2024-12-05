65e2a54fd52ae74a0d86fe0c87e0ebe139ab55ca
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-12-05 21:23:25 +0100

    add Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import ReactDom from 'react-dom/client';
 import App from './App';
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
